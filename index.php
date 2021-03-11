222bbb2b11c6f48163ab4b6129b246a31602db6c
amblonzi <ambvann@gmail.com>
2021-03-12 02:27:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "Hello World"; ?>
+<?php echo "string"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/css/uikit.min.css" />
+  </head>
+  <body>
+    <!-- Jquery -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit.min.js"></script>
+  <!-- Uikit -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.17/dist/js/uikit-icons.min.js"></script>
+  </body>
+</html>
